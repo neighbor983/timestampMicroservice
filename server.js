44f007ff6ab8c75a9dc3eb4536ae9c6065ef7d77
neighbor983 <kenneth.ray.baker@gmail.com>
2017-01-16 08:00:58 +0000

    serving up the index page.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
+var port = 8080;
+var path = require('path');
+
 
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080, function(){
-   console.log('listening on port 8080'); 
+app.listen(port, function(){
+   console.log('listening on port ' + port); 
 });
